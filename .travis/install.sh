de7f8b986dccb746ea586c28aea282beb1149135
Seth M. Larson <SethMichaelLarson@users.noreply.github.com>
2017-02-21 10:21:55 -0600

    Try installing dkms first?

--- .travis/install.sh
+++ .travis/install.sh
@@ -9,6 +9,7 @@ if [[ ! -z "$VIRTUALBOX" ]]; then
     wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
     wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
     sudo apt-get update
+    sudo apt-get install dkms
     sudo apt-get install virtualbox-5.1
 fi
 
